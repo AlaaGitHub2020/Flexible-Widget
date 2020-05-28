0d69ab0e2b9414b977f5447d7bd552a795a46f9c
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 15:36:22 +0530

    Flexible Widget example

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flexiblewidgetexample/my_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flexible Widget Simple Example',
+    home: MyHome(),
+  ));
+}
